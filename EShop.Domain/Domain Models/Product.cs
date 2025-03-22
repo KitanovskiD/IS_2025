3974cf7328793e81250c6a913d53fb995199cfb9
Dimitar Kitanovski <dimitar.kitanovski@finki.ukim.mk>
2025-03-22 19:54:39 +0100

    Intro to Onion (Domain, Web)

---
+++ EShop.Domain/Domain Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Domain.Domain_Models
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string? ProductName { get; set; }
+        [Required]
+        public string? ProductDescription { get; set; }
+        [Required]
+        public string? ProductImage { get; set; }
+        [Required]
+        public double ProductPrice { get; set; }
+        [Required]
+        public double Rating { get; set; }
+        public virtual ICollection<ProductInShoppingCart>? AllShoppingCarts { get; set; }
+
+    }
+}
---
+++ EShop.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EShop.Domain.Domain_Models;
+using EShop.Domain.Identity_Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<EShopApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public virtual DbSet<ProductInShoppingCart> ProductInShoppingCarts { get; set; }
+    }
+}
